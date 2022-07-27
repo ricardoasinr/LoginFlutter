df15a7940134b138892f8862ccee47354553a3d7
Ricardo Asin <ricardoasinr@gmail.com>
2022-07-27 15:12:04 -0500

    Añadiendo ReturnsecureToken

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -13,7 +13,8 @@ class AuthService extends ChangeNotifier {
   Future<String?> createUser(String email, String password) async {
     final Map<String, dynamic> authData = {
       'email': email,
-      'password': password
+      'password': password,
+      'returnSecureToken': true
     };
 
     final url =
@@ -36,7 +37,8 @@ class AuthService extends ChangeNotifier {
   Future<String?> loginUser(String email, String password) async {
     final Map<String, dynamic> authData = {
       'email': email,
-      'password': password
+      'password': password,
+      'returnSecureToken': true
     };
 
     final url = Uri.https(
